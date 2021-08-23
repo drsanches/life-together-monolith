42fea19419238efeb5a8bc0fa130c87e8705d49b
Alexander <dr.sanches3@gmail.com>
2021-08-23 15:24:15 +0300

    Removed unnecessary configurations

--- src/main/java/ru/drsanches/life_together/config/WebSecurityConfig.java
+++ src/main/java/ru/drsanches/life_together/config/WebSecurityConfig.java
@@ -1,18 +1,13 @@
 package ru.drsanches.life_together.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import ru.drsanches.life_together.security.AdminFilter;
 import ru.drsanches.life_together.security.TokenFilter;
-import ru.drsanches.life_together.service.utils.UserDetailsServiceImpl;
 import ru.drsanches.life_together.service.utils.UserPermissionService;
 import ru.drsanches.life_together.token.TokenService;
 import java.util.regex.Pattern;
@@ -30,9 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final Pattern ADMIN_URI_PATTERN = Pattern.compile("/h2-console.*|/swagger-ui.html.*");
 
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
-
-    @Autowired
     private TokenService tokenService;
 
     @Autowired
@@ -48,22 +40,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .anyRequest().permitAll();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 }
