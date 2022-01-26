9569dc9e5747108f3f6b825fbd9017f1ddec1f8f
Alexander <dr.sanches3@gmail.com>
2022-01-26 22:19:42 +0300

    ValidateCollection annotation replaced with Valid

--- src/main/java/ru/drsanches/life_together/app/data/debts/dto/SendMoneyDTO.java
+++ src/main/java/ru/drsanches/life_together/app/data/debts/dto/SendMoneyDTO.java
@@ -2,13 +2,13 @@ package ru.drsanches.life_together.app.data.debts.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.hibernate.validator.constraints.NotEmpty;
-import ru.drsanches.life_together.app.service.validation.annotation.ValidateCollection;
+import javax.validation.Valid;
 import java.util.List;
 
 public class SendMoneyDTO {
 
     @NotEmpty
-    @ValidateCollection(message = "transactions contains invalid objects")
+    @Valid
     @Schema(required = true)
     private List<SendTransactionDTO> transactions;
 
